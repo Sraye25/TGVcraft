feb22ca1ac46e8467fa04b8b5804ecf7d0c1409f
Sraye25 <bouchardthibault@gmail.com>
2015-05-28 22:35:59 +0200

    premier

---
+++ src/com/github/Sraye25/TGVcraft.java
@@ -0,0 +1,12 @@
+package com.github.Sraye25;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TGVcraft extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		System.out.println("Chargement de TGVcraft ...");
+	}
+}
