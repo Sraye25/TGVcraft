75e0e0cbff92950dccae957bc39bb36852b1b58d
Sraye25 <bouchardthibault@gmail.com>
2015-05-29 00:17:27 +0200

    premier

--- src/com/github/Sraye25/TGVcraft.java
+++ src/com/github/Sraye25/TGVcraft.java
@@ -1,12 +1,24 @@
 package com.github.Sraye25;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TGVcraft extends JavaPlugin
 {
+	FileConfiguration config = getConfig();
+    
+	public void loadConfiguration()
+	{
+        config.addDefault("vitesse_max",8); /*Vitesse en blocs par seconde*/
+        config.options().copyDefaults(true);
+		saveConfig();
+	}
+        
 	@Override
 	public void onEnable()
 	{
+		loadConfiguration();
 		System.out.println("Chargement de TGVcraft ...");
+		System.out.println("Vitesse max :" + config.getInt("vitesse_max"));
 	}
 }
