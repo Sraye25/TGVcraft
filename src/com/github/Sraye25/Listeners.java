e00cc5ea7141d1c490af9a2381ba5af73ce8c534
Sraye25 <bouchardthibault@gmail.com>
2015-05-30 00:54:46 +0200

    premier

--- src/com/github/Sraye25/Listeners.java
+++ src/com/github/Sraye25/Listeners.java
@@ -4,24 +4,18 @@ import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
 
 public class Listeners implements Listener
 {
 	
 	@EventHandler
-	public void onPlayerMove(PlayerMoveEvent event)
+	public void onVehicleMove(VehicleMoveEvent event)
 	{
-		// Get the player's location.
-	    Location loc = event.getPlayer().getLocation();
-	    // Sets loc to five above where it used to be. Note that this doesn't change the player's position.
-	    loc.setZ(loc.getZ() - 1);
-	    // Gets the block at the new location.
+	    Location loc = event.getVehicle().getLocation();
 	    Block b = loc.getBlock();
-	    
 	    @SuppressWarnings("deprecation")
 		int id_mat = b.getTypeId();
-	    
 	    System.out.println("Bloc : " + id_mat);
 	}
 }
