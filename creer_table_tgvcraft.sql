77140f37932a12474e02783f2c04e674cc7dd321
Sraye25 <bouchardthibault@gmail.com>
2015-05-31 20:45:40 +0200

    premier

---
+++ creer_table_tgvcraft.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Gare (
+	id_gare INTEGER NOT NULL PRIMARY KEY ,
+	nom VARCHAR(50) NOT NULL UNIQUE,
+	inter_gauche INTEGER,
+	inter_droite INTEGER,
+	dist_gauche INTEGER,
+	dist_droite INTEGER);
+
+CREATE TABLE Inter (
+	id_inter INTEGER NOT NULL PRIMARY KEY ,
+	gare_n INTEGER,
+	gare_s INTEGER,
+	gare_e INTEGER,
+	gare_o INTEGER);
+
